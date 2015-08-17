e643990f165f555eeb9370a8ee83d98aec9da411
johnjolet <john@jolet.net>
2015-08-17 02:11:49 +0000

    got it to the point where it encrypts the new files to a temp location, then sends to s3

--- thor.pl
+++ thor.pl
@@ -4,12 +4,21 @@ use Linux::Inotify2;
 use Config::File;
 use Proc::Daemon;
 use Getopt::Long;
+use GnuPG;
+use File::Basename;
+use Amazon::S3;
 
 my $configuration_file = "/usr/local/scripts/thor.cfg";
 my $config_hash = Config::File::read_config_file($configuration_file);
 my $uploads_dir = $config_hash->{uploads_dir};
 my $log_file = $config_hash->{log_file};
 my $pid_file = "/var/run/thor.pid";
+my $gpg_homedir = $config_hash->{gpg_homedir};
+my $gpg_recipient = $config_hash->{gpg_recipient};
+my $encrypted_dir = $config_hash->{encrypted_dir};
+my $archive_dir = $config_hash->{archive_dir};
+my $aws_access_key_id = $config_hash->{aws_access_key_id};
+my $aws_secret_access_key = $config_hash->{aws_secret_access_key};
 
 my $daemon = Proc::Daemon->new(
   pid_file     => $pid_file,
@@ -54,7 +63,7 @@ sub run {
       print $logfh "About to initialize thor daemon...\n";
       close $logfh;
 
-    $daemon->Init;
+    #$daemon->Init;
 
       my $notification = new Linux::Inotify2 or die "unable to watch $uploads_dir: $!";
       $notification->watch($uploads_dir, IN_CLOSE_WRITE, \&handle_new);
@@ -75,4 +84,80 @@ sub handle_new {
   open my $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
   print $logfh "Found " . $filename->fullname . "\n";
   close $logfh;
+
+  my $result = encrypt_file($filename->fullname);
+  open $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
+  print $logfh "Result of encryption: -->" . $result . "<--\n";
+  close $logfh;
+
+  if ($result) {
+      open my $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
+      print $logfh "About to send " . $filename->fullname . ".gpg to s3\n";
+      close $logfh;
+  }#end if
+
 } #end sub handle_new
+
+sub encrypt_file {
+  my $input_file = shift;
+  my($filename, $filedir, $fileext) = fileparse($input_file);
+  my $encrypted_file = $filename . ".gpg";
+  my $encrypted_path = $encrypted_dir . "/" . $encrypted_file;
+
+  open my $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
+  print $logfh "Encrypting " . $input_file . " to " . $encrypted_path . "\n";
+  close $logfh;
+
+  my $gpg = new GnuPG(
+    homedir => $gpg_homedir
+  );
+
+ 
+  eval {$gpg->encrypt (
+    plaintext => $input_file,
+    output    => $encrypted_path,
+    recipient => 'john@jolet.net'
+  )};
+
+  if ($@) {
+      open my $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
+      print $logfh "Unable to encrypt  " . $input_file . " to " . $encrypted_file . "$!\n";
+      close $logfh;
+      return 0;
+  } else {
+      open my $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
+      print $logfh "Successfully encrypted  " . $input_file . " to " . $encrypted_file . "$!\n";
+      close $logfh;
+  }#end else
+
+  send_to_s3($encrypted_path);
+
+} #end encrypt_file
+
+sub send_to_s3 {
+  my $input_file = shift;
+
+  my($filename, $filedir, $fileext) = fileparse($input_file);
+
+  open my $logfh, ">>", $log_file or die "Can't open the log file $log_file: $!";
+  print $logfh "Sending " . $input_file . " to s3...\n";
+  close $logfh;
+
+  my $s3 = Amazon::S3->new(
+      {   aws_access_key_id     => $aws_access_key_id,
+          aws_secret_access_key => $aws_secret_access_key,
+          retry                 => 1
+      }
+  );#end new s3
+
+  my $bucket = $s3->bucket("escrowfiles");
+
+  my $response = $bucket->add_key_filename(
+    $filename,
+    $input_file,
+    {
+      content_type => 'application/octet-stream',
+    }
+  );
+
+}#end send_to_s3
